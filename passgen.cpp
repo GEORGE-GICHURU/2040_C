a77f9b5b8c736d263c1acb9480e02f5d0e3a6fac
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-03-30 17:44:26 +0300

    display the input

--- passgen.cpp
+++ passgen.cpp
@@ -7,5 +7,18 @@ using namespace std;
 
 int main()
 {
-    srand(time(NULL));
+    srand(time(NULL));    // seed the random number generator with the current time;
+    string password = ""; // initialize an empty string for the passcode
+
+    // generate a random password of length 8
+    for (int i = 0; i < 8; i++)
+    {
+        int randomNum = rand() % 94 + 33;               // generate a random number between 33 and 126
+        char randomChar = static_cast<char>(randomNum); // convert the random number to a character
+        password += randomChar;                         // add the character to the password string
+    }
+
+    cout << "Your password is." << password << endl; // display the generated password
+
+    return 0;
 }
