b41cb3044c9abb118ac1c3db7e59d8c6e9614950
GEORGE-GICHURU <gichurugeorge092@gmail.com>
2023-03-31 21:01:55 +0300

    Read the students name, age and major

--- CRUD/CRUD.cpp
+++ CRUD/CRUD.cpp
@@ -26,3 +26,23 @@ void createStudent()
     outfile << s.name << "," << s.age << "," << s.major << endl;
     outfile.close();
 }
+
+void readStudents()
+{
+    ifstream infile("students.txt");
+    string line;
+    while (getline(infile, line))
+    {
+        Student s;
+        size_t pos = line.find(",");
+        s.name = line.substr(0, pos);
+        line.erase(0, pos + 1);
+        pos = line.find(",");
+        s.age = stoi(line.substr(0, pos));
+        line.erase(0, pos + 1);
+        s.major = line;
+        cout << "Name: " << s.name << ", Age:" << s.age << ", Major" << s.major << endl;
+    }
+
+    infile.close();
+}
